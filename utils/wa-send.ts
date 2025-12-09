7f27ff97166945827d4dece73229139204a1b61f
Achmad Rifai <achmad.rifai69@mandalafinance.com>
2025-12-09 15:04:36 +0700

    kafka + wa

--- utils/wa-send.ts
+++ utils/wa-send.ts
@@ -1,3 +1,20 @@
+import { SendWaReq } from "@/dto";
+import { getRepository } from "@/models";
+import { WaMessage } from "@/models/wa-message.model";
+import { sock, waConnected } from "@/wa";
+import { sleep } from "bun";
+
+interface WaSendingReq {
+    id: number;
+}
+
 export const waSending = async (msg: string) => {
+    while (!sock || !waConnected) { }
     console.log(msg);
+    const req: WaSendingReq = JSON.parse(msg);
+    const messageRepo = getRepository(WaMessage);
+    const message = await messageRepo.findOneByOrFail({ id: req.id });
+    const body = await SendWaReq.parseAsync(JSON.parse(message.value));
+    await sock.sendMessage(body.target, { text: body.textMessage });
+    await sleep(1000 * 10);
 };
